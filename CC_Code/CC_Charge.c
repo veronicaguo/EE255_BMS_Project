c7f22b1e31c77ac40a00edd90c38896cacba2625
Joseph Cohen <jos26coh@gmail.com>
2017-12-10 10:33:44 -0800

    CC Charge update 2

--- CC_Code/CC_Charge.c
+++ CC_Code/CC_Charge.c
@@ -9,9 +9,9 @@ float CC_Charge(float DUTY, float CURR)
 	cc_ch_error = IDES-CURR
 	cc_ch_integ = cc_ch_integ+cc_ch_error*DT
 	duty_new = (cc_ch_integ*KI_CC_CH)+(cc_ch_error*KP_CC_CH)
-	if duty_new >= DMAX
+	if (duty_new >= DMAX)
 		duty_new = DMAX
-	else if duty_new <= DMIN
+	else if (duty_new <= DMIN)
 		duty_new = DMIN
 	return duty_new
 }
