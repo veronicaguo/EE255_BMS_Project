41fff82034cab25b70133bd5ab7f1a13e2ada6ad
Joseph Cohen <jos26coh@gmail.com>
2017-12-10 10:30:03 -0800

    CC Charge update

--- CC_Code/CC_Charge.c
+++ CC_Code/CC_Charge.c
@@ -1,13 +1,17 @@
 #include "CC_Charge.h"
 
+float duty_new
+float cc_ch_error
+float cc_ch_integ
+
 float CC_Charge(float DUTY, float CURR)
 {
-	CC_CH_ERROR = IDES-CURR
-	INTEG = INTEG+CC_CH_ERROR*DT
-	DUTY_NEW = (INTEG*KI_CC_CH)+(ERROR*KP_CC_CH)
-	if DUTY_NEW >= DMAX
-		DUTY_NEW = DMAX
-	else if DUTY_NEW <= DMIN
-		DUTY_NEW = DMIN
-	return DUTY_NEW
+	cc_ch_error = IDES-CURR
+	cc_ch_integ = cc_ch_integ+cc_ch_error*DT
+	duty_new = (cc_ch_integ*KI_CC_CH)+(cc_ch_error*KP_CC_CH)
+	if duty_new >= DMAX
+		duty_new = DMAX
+	else if duty_new <= DMIN
+		duty_new = DMIN
+	return duty_new
 }
--- CC_Code/CC_Charge.h
+++ CC_Code/CC_Charge.h
@@ -1,17 +1,8 @@
-#ifndef CC_CH_ERROR
-#define CC_CH_ERROR
-
 #ifndef IDES_CH
 #define IDES_CH 2
 
-#ifndef CC_CH_INTEG
-#define CC_CH_INTEG
-
 #ifndef DT
 #define DT 1e-6
-
-#ifndef DUTY_NEW
-#define DUTY_NEW
 
 #ifndef KI_CC_CH
 #define KI_CC_CH 1
