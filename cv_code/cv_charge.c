50dba3cdbe404d91b8c9362fbe22dfec03fdf5b8
Veronica Guo <gsy0814@hotmail.com>
2017-12-10 10:36:31 -0800

    Implemented constant voltage charging.

---
+++ cv_code/cv_charge.c
@@ -0,0 +1,21 @@
+/*!
+  EE255 BMS constant voltage charging.
+*/
+
+#include "cv_charge.h"
+
+float cv_charge(float duty, float vol_pack)
+{
+	error_cv = V_REF - vol_pack
+	integ = integ + error_cv * DT
+	duty_new = (integ * KI_CV_CH) + (error_cv * KP_CV_CH)
+
+	if (duty_new >= DMAX){
+		duty_new = DMAX
+	}
+	else if (duty_new <= DMIN){
+		duty_new = DMIN
+	}
+	
+	return duty_new
+}
