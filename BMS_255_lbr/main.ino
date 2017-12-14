9b8972395275807f1c27cc8529f72a615612406c
Joseph Cohen <jos26coh@gmail.com>
2017-12-13 22:03:20 -0800

    sdfjksdhfsdjfdsk

--- BMS_255_lbr/main.ino
+++ BMS_255_lbr/main.ino
@@ -17,15 +17,20 @@
 #include "PWM_GEN.h"
 
 float duty = 0;
-float cell_voltage[4];
-float temp_value[2];
-float* output = (float *) malloc(7*sizeof(float));
+float cell_voltage;
+float temp_value;
+//float* outputvals = (float *) malloc(7*sizeof(float));
 float cell_current;
 int mode;
 int i;
-uint16_t cell_voltage_meas[50][4];
-uint16_t temp_voltage_meas[50][2];
+uint16_t cell_voltage_meas[50];
+uint16_t temp_voltage_meas[50];
 uint16_t cell_current_meas[50];
+int baud = 9600;
+
+void setup() {
+  Serial.begin(baud);
+}
 
 int main(){
 	
@@ -46,19 +51,21 @@ int main(){
 			cell_voltage_meas[i] = LTC_cell_voltage_meas();
 			temp_voltage_meas[i] = LTC_temp_voltage_meas();
 			// need to create ``LTC_cell_current_meas()`` in initialization.c
-			cell_current_meas[i] = LTC_cell_current_meas();
+			//cell_current_meas[i] = LTC_cell_current_meas();
 		}
-		cell_voltage = vconv(cell_voltage_meas);
-		temp_value = tconv(temp_voltage_meas);
-		cell_current = iconv(cell_current_meas);
+		cell_voltage = vconv(*cell_voltage_meas);
+		temp_value = tconv(*temp_voltage_meas);
+		//cell_current = iconv(cell_current_meas);
 
-		memcpy(output, cell_voltage, 4*sizeof(float));
-		memcpy(output+4, temp_value, 2*sizeof(float));
-		memcpy(output+6, temp_value, 1*sizeof(float));
+		//memcpy(outputvals, cell_voltage, sizeof(float));
+		//memcpy(outputvals+4, temp_value, sizeof(float));
+		//memcpy(outputvals+6, cell_current, 1*sizeof(float));
+    Serial.print(cell_voltage);
+    Serial.println(temp_value);
 
-		SPI.transfer(output);
+		//Serial.println(outputvals);
 
-		duty = check_state(cell_voltage, cell_current, mode);	
+		//duty = check_state(cell_voltage, cell_current, mode);	
 	}
 
 	return 0;
